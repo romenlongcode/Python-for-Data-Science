d1f314823d5e418738fc253d65248eb6ce3e08bc
Moirangthem Romenkumar Singh <83605713+romenrake@users.noreply.github.com>
2022-09-09 20:01:54 +0530

    Add files via upload

---
+++ plist.read.py
@@ -0,0 +1,15 @@
+my_list=['Ram','Kahul','James','Tony']
+#checking type
+print(type(my_list))
+#reading values of the list
+print(my_list)
+print(my_list[2])
+print(my_list[-1])
+print(my_list[1:3])
+print(my_list[:2])
+print(my_list[2:])
+#extend the  list
+my_list1=['Angou','Thoi','Roni','Tania','Joni']
+print(my_list1)
+my_list.extend(my_list1)
+print(my_list)
