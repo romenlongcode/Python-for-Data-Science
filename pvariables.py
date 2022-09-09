80719bff360392c890345b7d65ed1edf2790cf86
Moirangthem Romenkumar Singh <83605713+romenrake@users.noreply.github.com>
2022-09-09 20:01:18 +0530

    Add files via upload

---
+++ pvariables.py
@@ -0,0 +1,16 @@
+print(3**3)
+print(3*8)
+print(4**3)
+print(12%2)
+print(12%3)
+print(12%4)
+print(12%5)
+print(12%7)
+print(34//4)
+import math
+print(math.sin(45))
+print(math.sin(90))
+/ for i in range(10):
+ if i%2==1:
+  print(i)
+ /
